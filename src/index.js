c60af97e27aafda08fd269b9d9e7f6dcd26a42d4
khan <husnakhan551@gmail.ccom>
2023-09-13 22:10:14 +0530

    Committing existing changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "../src/App";
 import { BrowserRouter } from "react-router-dom";
 import {configureChains,mainnet , WagmiConfig,createClient} from "wagmi";
 import {publicProvider} from "wagmi/providers/public";
